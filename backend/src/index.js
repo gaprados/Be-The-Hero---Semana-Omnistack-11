6c853d577c2c716806f5a5b0da02a7732e5c05a3
Gabriel Prado <gaprados3@gmail.com>
2020-03-25 16:54:18 -0300

    Backend da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const port = 3333;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(port, function() {
+    console.log(`Server running on port ${port}...`);
+});
